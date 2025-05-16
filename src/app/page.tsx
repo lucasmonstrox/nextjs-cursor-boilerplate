8546d494a021bb55da712d644273e3ae374c82b1
lucasmonstrox <joao.galiano.silva@gmail.com>
2025-05-16 16:22:35 +0100

    refactor: improve code style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center p-8">
-      <div className="max-w-md w-full space-y-6">
-        Hello World
-      </div>
+      <div className="max-w-md w-full space-y-6">Hello World</div>
     </main>
   );
 }
