1c366c571623076eee50ba2447c000a8107862b3
lucasmonstrox <joao.galiano.silva@gmail.com>
2025-05-16 22:41:44 +0100

    refactor(cursor): improve more rules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
 import { Toaster } from '@/components/ui/sonner';
+import { Navbar } from '@/components/ui/navbar';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
+        <Navbar />
         {children}
         <Toaster />
       </body>
