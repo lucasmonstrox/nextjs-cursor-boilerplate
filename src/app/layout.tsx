14f7c7f14694273d86caa1ed2f89d3610d33370d
lucasmonstrox <joao.galiano.silva@gmail.com>
2025-05-18 00:21:28 +0100

    refactor: improve coding style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
 
 import { Toaster } from '@/components/ui/sonner';
 import { Navbar } from '@/components/ui/navbar';
+import './globals.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
